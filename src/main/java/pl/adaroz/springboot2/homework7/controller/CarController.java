2c1fbddcd4ec778c8899e46e913ee8ba3e618e79
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-02 19:50:10 +0200

    Controller - method getAllCars()

---
+++ src/main/java/pl/adaroz/springboot2/homework7/controller/CarController.java
@@ -0,0 +1,31 @@
+package pl.adaroz.springboot2.homework7.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.adaroz.springboot2.homework7.model.Car;
+import pl.adaroz.springboot2.homework7.service.CarService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+
+    private CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping("")
+    public String getAllCars(Model model) {
+        List<Car> allCars = carService.findAll();
+        model.addAttribute("allCars", allCars);
+        return "/cars";
+    }
+
+}
