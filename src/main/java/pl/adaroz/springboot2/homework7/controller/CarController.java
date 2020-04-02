32d74be939c04835175482d471db8b179dcf18b1
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-02 21:30:19 +0200

    Deleting a car

--- src/main/java/pl/adaroz/springboot2/homework7/controller/CarController.java
+++ src/main/java/pl/adaroz/springboot2/homework7/controller/CarController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.adaroz.springboot2.homework7.model.Car;
 import pl.adaroz.springboot2.homework7.service.CarService;
 
@@ -26,6 +27,12 @@ public class CarController {
         List<Car> allCars = carService.findAll();
         model.addAttribute("allCars", allCars);
         return "/cars";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCar(@RequestParam long id) {
+        carService.deleteCar(id);
+        return "redirect:/cars";
     }
 
 }
