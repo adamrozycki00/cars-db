c1b121f6aad39f0bd5b911f2083633045adc90ea
Adam Rozycki <adamrozycki00@gmail.com>
2020-04-04 17:50:19 +0200

    Some immaterial changes

--- src/main/java/pl/adaroz/springboot2/homework7/dao/CarDao.java
+++ src/main/java/pl/adaroz/springboot2/homework7/dao/CarDao.java
@@ -1,16 +1,14 @@
 package pl.adaroz.springboot2.homework7.dao;
 
-import org.springframework.stereotype.Component;
 import pl.adaroz.springboot2.homework7.model.Car;
 
 import java.util.List;
 
-@Component
 public interface CarDao {
 
-    public void saveCar(String make, String model, String colour, Long year);
+    void saveCar(String make, String model, String colour, Long year);
 
-    public void saveCar(Car newCar);
+    void saveCar(Car newCar);
 
     List<Car> findAll();
 
